ec3b2aca25666267bbd4e7e213ed93c7b6f9efcc
shravnisingh <shravanisingh22@gmail.com>
2024-12-12 23:15:50 +0530

    UC-02

--- src/Max.java
+++ src/Max.java
@@ -2,20 +2,36 @@ import java.util.Scanner;
 
 public class Max {
 
-    private Integer a, b, c;
+    private Integer int1, int2, int3;
+    private Float float1, float2, float3;
 
-    public Max(Scanner sc) {
+    public void inputMaxInt(Scanner sc) {
         System.out.println("Enter the First Number:");
-        this.a = sc.nextInt();
+        this.int1 = sc.nextInt();
         System.out.println("Enter the Second Number:");
-        this.b = sc.nextInt();
+        this.int2 = sc.nextInt();
         System.out.println("Enter the Third Number:");
-        this.c = sc.nextInt();
+        this.int3 = sc.nextInt();
     }
 
-    public Integer getMax() {
-        Integer max = (a.compareTo(b) >= 0) ? a : b;
-        max = (max.compareTo(c) >= 0) ? max : c;
+    public void inputMaxFloat(Scanner sc) {
+        System.out.println("Enter the First float Number:");
+        this.float1 = sc.nextFloat();
+        System.out.println("Enter the Second float Number:");
+        this.float2 = sc.nextFloat();
+        System.out.println("Enter the Third float Number:");
+        this.float3 = sc.nextFloat();
+    }
+
+    public Integer getMaxInteger() {
+        Integer max = (int1.compareTo(int2) >= 0) ? int1 : int2;
+        max = (max.compareTo(int3) >= 0) ? max : int3;
+        return max;
+    }
+
+    public Float getMaxFloat() {
+        Float max = (float1.compareTo(float2) >= 0) ? float1 : float2;
+        max = (max.compareTo(float3) >= 0) ? max : float3;
         return max;
     }
 }
